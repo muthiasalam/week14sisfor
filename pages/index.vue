6cfc053611fbfbf79b1ace06348af9b769cb9dcb
marcelindasr <marcelindasr@gmail.com>
2023-12-09 14:58:59 +0800

    nuxtpage

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <Headerr/>
+    <Navbar/>
+    <Banner/>
+    <Sambutan/>
+    <Galeri/>
+    <Dharma/>
+    <Berita/>
+    <Informasi/>
+    <Tour/>
+    <Footerr/>
+  </template>
+  
+  <script setup>
+  import Headerr from '../src/components/Headerr.vue'
+  import Navbar from '../src/components/Navbar.vue'
+  import Banner from '../src/components/Banner.vue'
+  import Tour from '../src/components/Tour.vue'
+  import Sambutan from '../src/components/Sambutan.vue'
+  import Galeri from '../src/components/Galeri.vue'
+  import Dharma from '../src/components/Dharma.vue'
+  import Berita from '../src/components/Berita.vue'
+  import Informasi from '../src/components/Informasi.vue' 
+  import Footerr from '../src/components/Footerr.vue'
+  
+  </script>
+  
+  <style lang="css" scoped></style>
