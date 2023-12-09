d74a65bef4eb597cf0da081a1594ca7041f26fab
marcelindasr <marcelindasr@gmail.com>
2023-12-09 09:56:23 +0800

    fetch berita

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/src/assets/css/main.css'],
+  css: ['~/src/assets/css/main.css'], // Ubah path CSS sesuai dengan struktur folder yang benar
+  modules: ["nuxt-directus"], 
+  directus: { 
+    url: "http://localhost:8055/", // Ganti dengan URL proyek Directus yang benar
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
--- src/components/Berita.vue
+++ src/components/Berita.vue
@@ -1,5 +1,5 @@
 <template>
-    <div
+  <div
     class="Berita mb-5 ml-8 mr-8 flex flex-wrap items-start justify-start pb-4"
   >
     <div class="judul text-2xl mb-4 mt-8 font-semibold text-black">BERITA</div>
@@ -7,13 +7,17 @@
       <div
         class="berita-content -mx-4 mr-4 flex flex-wrap items-start justify-start pb-4 md:justify-center"
       >
-        <div class="mb-4 ml-4 w-full bg-slate-100 md:w-1/3 lg:w-1/5">
+        <div
+          v-for="x in data.data"
+          :key="x.id"
+          class="mb-4 ml-4 w-full bg-slate-100 md:w-1/3 lg:w-1/5"
+        >
           <a href="#" class="berita-item1 h-65 mr-10 w-64">
             <div class="relative h-40 w-full">
               <img
                 class="h-full w-full"
-                src="../assets/berita/imgb1.jpg"
-                alt="News Image 1"
+                :src="`http://localhost:8055/assets/` + x?.thumbnail"
+                alt="News Image"
               />
               <div
                 class="absolute left-0 top-0 h-full w-full bg-red-600 opacity-30 hover:bg-blue-400"
@@ -21,90 +25,11 @@
             </div>
             <div class="judul-berita1 w-50 relative m-3 h-20 hover:bg-blue-100">
               <p class="line-clamp-3 text-xs font-medium text-neutral-700">
-                Fakultas Teknik Sukses Gelar Konferensi Internasional 2023
+                {{ x?.judul }}
               </p>
               <div class="absolute bottom-0 left-0">
                 <p class="text-xs font-normal text-neutral-700">
-                  13 Oktober 2023
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div class="mb-4 ml-4 w-full bg-slate-100 md:w-1/3 lg:w-1/5">
-          <a href="#" class="berita-item2 h-65 mr-10 w-64 bg-slate-100">
-            <div class="relative h-40 w-full">
-              <img
-                class="h-full w-full"
-                src="../assets/berita/imgb2.jpg"
-                alt="News Image 2"
-              />
-              <div
-                class="absolute left-0 top-0 h-full w-full bg-red-600 opacity-30 hover:bg-blue-400"
-              ></div>
-            </div>
-            <div class="judul-berita2 w-50 relative m-3 h-20 hover:bg-blue-100">
-              <p class="line-clamp-3 text-xs font-medium text-neutral-700">
-                Fakultas Teknik Universitas Hasanuddin Gelar Rapat Koordinasi
-                Ex-Post Evaluasi Project JICA.
-              </p>
-              <div class="absolute bottom-0 left-0">
-                <p class="text-xs font-normal text-neutral-700">
-                  13 Oktober 2023
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div class="mb-4 ml-4 w-full bg-slate-100 md:w-1/3 lg:w-1/5">
-          <a href="#" class="berita-item3 h-65 mr-10 w-64 bg-slate-100">
-            <div class="relative h-40 w-full">
-              <img
-                class="h-full w-full"
-                src="../assets/berita/imgb3.jpg"
-                alt="News Image 3"
-              />
-              <div
-                class="absolute left-0 top-0 h-full w-full bg-red-600 opacity-30 hover:bg-blue-400"
-              ></div>
-            </div>
-            <div class="judul-berita3 w-50 relative m-3 h-20 hover:bg-blue-100">
-              <p class="line-clamp-3 text-xs font-medium text-neutral-700">
-                Kedatangan Profesor Dari Jepang, Departemen Teknik Industri
-                Fakultas Teknik Universitas Hasanuddin Laksanakan International
-                Guest Lecturer
-              </p>
-              <div class="absolute bottom-0 left-0">
-                <p class="text-xs font-normal text-neutral-700">
-                  11 Oktober 2023
-                </p>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div class="mb-4 ml-4 w-full bg-slate-100 md:w-1/3 lg:w-1/5">
-          <a href="#" class="berita-item4 h-65 mr-12 w-64 bg-slate-100">
-            <div class="relative h-40 w-full">
-              <img
-                class="h-full w-full"
-                src="../assets/berita/imgb4.jpg"
-                alt="News Image 4"
-              />
-              <div
-                class="absolute left-0 top-0 h-full w-full bg-red-600 opacity-30 hover:bg-blue-400"
-              ></div>
-            </div>
-            <div class="judul-berita4 w-50 relative m-3 h-20 hover:bg-blue-100">
-              <p class="line-clamp-3 text-xs font-medium text-neutral-700">
-                Fakultas Teknik Universitas Hasanuddin Jadi Tuan Rumah Temu
-                Ilmiah Ikatan Peneliti Lingkungan Binaan Indonesia ke - 11
-              </p>
-              <div class="absolute bottom-0 left-0">
-                <p class="text-xs font-normal text-neutral-700">
-                  11 Oktober 2023
+                  {{ formatDate(x?.date_created) }}
                 </p>
               </div>
             </div>
@@ -114,11 +39,25 @@
 
       <div class="flex justify-end">
         <p class="text-xs font-light text-gray-800">
-          <span class="more-news text-xs font-light hover:text-blue-600"
+          <a href="#"><span class="more-news text-xs font-light hover:text-blue-600"
             >LIHAT BERITA LAIN ></span
-          >
+          ></a>
         </p>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+const data = await $fetch("http://localhost:8055/items/berita");
+
+console.log(data);
+// http://localhost:8055/items/berita
+
+const formatDate = (date) => {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  const formattedDate = new Date(date);
+  return formattedDate.toLocaleDateString("id-ID", options);
+};
+
+</script>
